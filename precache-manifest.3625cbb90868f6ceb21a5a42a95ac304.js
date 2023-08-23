6b7b8e1e52d01a61173dae2ad74a0d96db4a3b87
tusmitshah <tusmitshah@gmail.com>
2023-08-23 11:01:09 +0530

    Updates

---
+++ precache-manifest.3625cbb90868f6ceb21a5a42a95ac304.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "488971106d47e192c94a",
+    "url": "/registration-form/static/css/main.25749166.chunk.css"
+  },
+  {
+    "revision": "488971106d47e192c94a",
+    "url": "/registration-form/static/js/main.48897110.chunk.js"
+  },
+  {
+    "revision": "54087e177e7c5a193df0",
+    "url": "/registration-form/static/js/1.54087e17.chunk.js"
+  },
+  {
+    "revision": "a62c13b1ed67c0eb3e37",
+    "url": "/registration-form/static/js/2.a62c13b1.chunk.js"
+  },
+  {
+    "revision": "778366223441fa4288c3",
+    "url": "/registration-form/static/js/runtime~main.77836622.js"
+  },
+  {
+    "revision": "8616c42cac01d3e4a2852b7acbc66aaa",
+    "url": "/registration-form/static/media/logo.8616c42c.jpeg"
+  },
+  {
+    "revision": "22976d826278ae1dbc7e0b358684ab83",
+    "url": "/registration-form/index.html"
+  }
+];
